5e727d41845f025d43b95141e62336cedbab5773
Joy <github.joysaha@gmail.com>
2024-02-16 16:36:51 +0600

    discount section complete

--- index.js
+++ index.js
@@ -32,4 +32,29 @@ for(let i = 0; i < cards.length; i++){
     });
 };
 
-const btn = 
+const btn = document.getElementById('apply-btn');
+btn.addEventListener('click', function(){
+    
+    const couponElement = document.getElementById("input-field").value;
+    const couponCode = couponElement.split(" ").join("").toUpperCase();
+
+    console.log(couponCode);
+    if(totalPrice >= 200){
+        if(couponCode === "SELL200"){
+            const discountElement = document.getElementById("discountPrice");
+            const discountAmount = totalPrice * 0.2;
+            discountElement.innerText = discountAmount.toFixed(2);
+
+
+            const restTotal = document.getElementById("total");
+            restTotal.innerText =totalPrice - discountAmount.toFixed(2);
+            document.getElementById("input-field").value = " ";
+        }else{
+            alert("Invalid Coupon");
+            document.getElementById("input-field").value = " ";
+        }
+    }else{
+        alert("Please at least $200 khoroch koro");
+        document.getElementById("input-field").value = " ";
+    }
+});
